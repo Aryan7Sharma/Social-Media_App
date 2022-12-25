e4586c318ecba2b70b12eaac2aa0f23d1e8ee73a
Aryan7Sharma <aryansharma2003march@gmail.com>
2022-12-25 14:36:16 +0530

    c

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = None
+    id_user = None
+    bio = None
+    profileimg = None
+    location = None
+
