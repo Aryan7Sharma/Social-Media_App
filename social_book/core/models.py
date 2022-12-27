9fc2ded812b9370b9b343edf19db5e4f3781ff19
Aryan7Sharma <aryansharma2003march@gmail.com>
2022-12-27 08:50:02 +0530

    c

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -5,6 +5,6 @@ class Profile(models.Model):
     user = None
     id_user = None
     bio = None
-    profileimg = None
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.jpg')
     location = None
 
