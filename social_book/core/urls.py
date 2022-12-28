ce8891828e4f92a46932f562a085c25abd1293b0
Aryan7Sharma <aryansharma2003march@gmail.com>
2022-12-28 17:54:46 +0530

    c

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
     path('setting', views.setting, name='setting'),
+    path('upload', views.upload, name='upload')
 ]
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    if user_object:
+        print(user_object)
+        user_profile = Profile.objects.get(user=user_object)
+    else:
+        user_profile = {}
+    return render(request, 'index.html',{'user_profile':user_profile})
 
 # create sign-up function
 def signup(request):
@@ -28,12 +35,14 @@ def signup(request):
                 user.save()
 
                 #Log user in and redirect to setting page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 #create a Profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('setting')
 
         else:
             messages.info(request, 'Password Not Matched Try Again Please!')
@@ -68,4 +77,31 @@ def logout(request):
 # settings
 @login_required(login_url='signin') #decorator
 def setting(request):
-    return render(request, 'setting.html')
+    user_profile = Profile.objects.get(user=request.user)
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+        if request.FILES.get('image'):
+            image = request.FILES.get('image')
+        bio = request.POST['bio']
+        location = request.POST['location']
+        user_profile.profileimg = image
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+        return redirect('setting')
+    return render(request, 'setting.html', {'user_profile':user_profile})
+
+# upload posts page
+@login_required(login_url=signin)
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
