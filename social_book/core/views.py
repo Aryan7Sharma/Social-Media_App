9e63b0d935e1248166ae2ff1bdb9610d333874c0
Aryan7Sharma <aryansharma2003march@gmail.com>
2022-12-30 08:17:51 +0530

    c

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 from itertools import chain
+import random
 
 # Create your views here.
 @login_required(login_url='signin')
